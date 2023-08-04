190823addb712c74d37e94c46379b28aaddd3ac7
Jacob Harrington <Jacob@Juelias-MacBook-Pro-2.local>
2023-08-03 20:36:39 -0700

    Small app assignment + begin dicee project from clone

---
+++ I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Jacob Harrington on 8/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
